8d4bcd4869de85d9667028c5211b173a2b309730
kangjy <kangjy@kangjy-sitech>
2016-09-09 10:33:03 +0800

    更新Deferred对象的一些注释，同时修改了样式文件，字体改为微软雅黑

--- src/02-deferred.js
+++ src/02-deferred.js
@@ -191,7 +191,7 @@ jQuery.Callbacks = function( options ) {
         return self;
 };
 jQuery.extend({
-
+	// 在jquery.callbacks基础上，为回调函数增加了状态，并提供多个状态的回调函数
     Deferred: function( func ) {
             var tuples = [
                             // action, add listener, listener list, final state
@@ -200,41 +200,46 @@ jQuery.extend({
                             [ "notify", "progress", jQuery.Callbacks("memory") ]
                     ],
                     state = "pending",
+                    // 异步队列的只读副本
                     promise = {
-                            state: function() {
+                            // 返回异步队列的状态
+            				state: function() {
                                     return state;
                             },
+                            // 用于将函数添加到成功队列和失败队列中
                             always: function() {
                                     deferred.done( arguments ).fail( arguments );
                                     return this;
                             },
+                            // 接收三个可选的过滤函数为参数，用于过滤当前异步队列的状态和参数，并返回一个新的异步队列只读副本，可以理解为一个defer过滤器
                             then: function( /* fnDone, fnFail, fnProgress */ ) {
                                     var fns = arguments;
                                     return jQuery.Deferred(function( newDefer ) {
                                             jQuery.each( tuples, function( i, tuple ) {
                                                     var action = tuple[ 0 ],
                                                             fn = fns[ i ];
-                                                    // deferred[ done | fail | progress ] for forwarding actions to newDefer
                                                     deferred[ tuple[1] ]( jQuery.isFunction( fn ) ?
                                                             function() {
                                                                     var returned = fn.apply( this, arguments );
                                                                     if ( returned && jQuery.isFunction( returned.promise ) ) {
+                                                                    	//返回值是defer对象，将newDefer的执行放到该defer对象的的回调函数列表里
                                                                             returned.promise()
                                                                                     .done( newDefer.resolve )
                                                                                     .fail( newDefer.reject )
                                                                                     .progress( newDefer.notify );
                                                                     } else {
+                                                                    		// 返回值returned作为参数，newDefer依赖外层执行结果
                                                                             newDefer[ action + "With" ]( this === deferred ? newDefer : this, [ returned ] );
                                                                     }
                                                             } :
+                                                            // 如果传入的参数不是函数，newDefer的执行依赖外层调用者的状态
                                                             newDefer[ action ]
                                                     );
                                             });
                                             fns = null;
                                     }).promise();
                             },
-                            // Get a promise for this deferred
-                            // If obj is provided, the promise aspect is added to the object
+                            // 如果没有传入obj，返回当前异步队列只读副本promise ,如果传入参数obj，将只读的副本promise的方法添加到obj中
                             promise: function( obj ) {
                                     return obj != null ? jQuery.extend( obj, promise ) : promise;
                             }
@@ -244,7 +249,7 @@ jQuery.extend({
             // Keep pipe for back-compat
             promise.pipe = promise.then;
 
-            // Add list-specific methods
+            // 为异步队列增加相应得回调函数列表方法
             jQuery.each( tuples, function( i, tuple ) {
                     var list = tuple[ 2 ],
                             stateString = tuple[ 3 ];
@@ -252,53 +257,54 @@ jQuery.extend({
                     // promise[ done | fail | progress ] = list.add
                     promise[ tuple[1] ] = list.add;
 
-                    // Handle state
                     if ( stateString ) {
+                    		//在异步队列添加三个成功/失败的回调方法，分别设置其状态，并禁用另外一个回调函数列表，同时锁定通知回调列表
                             list.add(function() {
                                     // state = [ resolved | rejected ]
                                     state = stateString;
 
-                            // [ reject_list | resolve_list ].disable; progress_list.lock
+                            // 这一块写法很有记录，通过i^1来取反，让另为一个回调列表禁用
                             }, tuples[ i ^ 1 ][ 2 ].disable, tuples[ 2 ][ 2 ].lock );
                     }
 
-                    // deferred[ resolve | reject | notify ] = list.fire
+                    // 触发成功，失败，通知消息回调函数列表的触发方法
                     deferred[ tuple[0] ] = list.fire;
                     deferred[ tuple[0] + "With" ] = list.fireWith;
             });
 
-            // Make the deferred a promise
+            // 为异步队列添加只读方法
             promise.promise( deferred );
 
-            // Call given func if any
+            // 如果传入func，把deferred设置为上下文，同时把deferred作为参数传进去，这样在func执行过程，仍然可以调用异步队列
             if ( func ) {
                     func.call( deferred, deferred );
             }
 
-            // All done!
             return deferred;
     },
 
-    // Deferred helper
+    // 传入多个异步队列为参数，返回一个新的主异步队列的只读副本，将追踪传入异步队列的所有状态
     when: function( subordinate /* , ..., subordinateN */ ) {
             var i = 0,
                     resolveValues = core_slice.call( arguments ),
                     length = resolveValues.length,
 
-                    // the count of uncompleted subordinates
+                    // 如果length=1，则remaining=1，如果length不等于1，参数为deferred对象，则返回参数的长度
                     remaining = length !== 1 || ( subordinate && jQuery.isFunction( subordinate.promise ) ) ? length : 0,
 
-                    // the master Deferred. If resolveValues consist of only a single Deferred, just use that.
+                    // 如果参数只有一个，则使用已有的deferred对象，否则则新建一个
                     deferred = remaining === 1 ? subordinate : jQuery.Deferred(),
 
-                    // Update function for both resolve and progress values
                     updateFunc = function( i, contexts, values ) {
+                    	    // 返回一个函数，为了创建一个闭包，保持i的值，返回到数组
                             return function( value ) {
                                     contexts[ i ] = this;
+                                    // 返回多个参数的情况，则直接转换成数组，返回一个话，直接返回当前值
                                     values[ i ] = arguments.length > 1 ? core_slice.call( arguments ) : value;
                                     if( values === progressValues ) {
                                             deferred.notifyWith( contexts, values );
                                     } else if ( !( --remaining ) ) {
+                                    		// 所有的Deferred的对象都已经触发完
                                             deferred.resolveWith( contexts, values );
                                     }
                             };
@@ -306,13 +312,14 @@ jQuery.extend({
 
                     progressValues, progressContexts, resolveContexts;
 
-            // add listeners to Deferred subordinates; treat others as resolved
+            // 处理多个参数的情况
             if ( length > 1 ) {
                     progressValues = new Array( length );
                     progressContexts = new Array( length );
                     resolveContexts = new Array( length );
                     for ( ; i < length; i++ ) {
                             if ( resolveValues[ i ] && jQuery.isFunction( resolveValues[ i ].promise ) ) {
+                            		// 只处理deferred的对象
                                     resolveValues[ i ].promise()
                                             .done( updateFunc( i, resolveContexts, resolveValues ) )
                                             .fail( deferred.reject )
@@ -323,7 +330,7 @@ jQuery.extend({
                     }
             }
 
-            // if we're not waiting on anything, resolve the master
+            // 针对上面最后一个循环走到else分支的场景，同时可以看到返回回调列表返回值的数组
             if ( !remaining ) {
                     deferred.resolveWith( resolveContexts, resolveValues );
             }
